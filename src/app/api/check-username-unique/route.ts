9babb0c3760f44904d5622c15209f2d8b98a4696
DeveshSharma2004 <devesh00coding@gmail.com>
2024-09-19 09:12:32 +0530

    add some routes

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -2,7 +2,6 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { z } from "zod";
 import { usernameValidation } from "@/schemas/signUpSchema";
-
 const UsernameQuerySchema = z.object({
     username: usernameValidation
 })
